09b36fb9eacf475df03d9759ac33704ea0a413b9
Tom Cartwright <tecartwright@gmail.com>
2015-12-22 14:43:53 +0000

    Update chat example with binary file transfer

--- examples/chat/server.js
+++ examples/chat/server.js
@@ -1,6 +1,6 @@
 var ecstatic = require('ecstatic');
 var server = require('http').createServer(
-  ecstatic({ root: __dirname })
+  ecstatic({ root: __dirname, handleError: false })
 );
 var p2pserver = require('socket.io-p2p-server').Server
 var io = require('socket.io')(server);
@@ -15,6 +15,11 @@ io.on('connection', function(socket) {
   socket.on('peer-msg', function(data) {
     console.log('Message from peer: %s', data);
     socket.broadcast.emit('peer-msg', data);
+  })
+
+  socket.on('peer-file', function(data) {
+    console.log('File from peer: %s', data);
+    socket.broadcast.emit('peer-file', data);
   })
 
   socket.on('go-private', function(data) {
